0db3f54fa0c2f9aa329bcd5d37e3b8e0f12a6d13
a-vilmin <adam.vilmin@ibm.com>
2019-06-23 18:36:48 -0500

    ready to test

--- feed_checker.py
+++ feed_checker.py
@@ -1,4 +1,5 @@
 import feedparser
+from datetime import datetime, timedelta
 
 def check_activity(feed_dict, length_of_time):
     none_in_interval = []
@@ -9,5 +10,12 @@ def check_activity(feed_dict, length_of_time):
                 latest_entry = loaded_feed.entries[0]
             except KeyError:
                 print("There was an error loading " + feed)
-                continue 
-    return []
+                continue
+
+            # Get date, check if its from before the time interval, add name to list if it is
+            latest_entry = datetime.strptime(latest_entry.published, "%a, %d %b %Y %H:%M:%S -%f")
+
+            if latest_entry < datetime.now() - timedelta(days=length_of_time):
+                none_in_interval += [company]
+                break
+    return none_in_interval
